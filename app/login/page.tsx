51ecd1d1a8afe69f8d9a615ba3d9de6da412768f
Ayemo Andrew <ayemoandrewgold@gmail.com>
2026-05-30 12:34:12 +0100

    testing Live API

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -55,7 +55,7 @@ export default function StudentLoginPage() {
     }
     setLoading(true)
     try {
-      const res  = await fetch('http://127.0.0.1:8000/api/auth/login/', {
+      const res  = await fetch('https://yct-mental-health-counselling-platform.onrender.com/api/auth/login/', {
         method:  'POST',
         headers: { 'Content-Type': 'application/json' },
         body:    JSON.stringify({ email: identifier, password }),
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -114,7 +114,7 @@ export default function RegisterPage() {
     if (err) { toast.error(err); return }
     setLoading(true)
     try {
-      const res = await fetch('http://127.0.0.1:8000/api/auth/register/', {
+      const res = await fetch('https://yct-mental-health-counselling-platform.onrender.com/api/auth/register/', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
